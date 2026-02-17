bca6b75d5e8667701f556b384fbd42d11449e06f
Raiyan kammal <supremealpha1211@gmail.com>
2026-02-17 15:49:27 +0530

    Updated UC2

--- DEMO.java
+++ DEMO.java
@@ -1,21 +1,7 @@
-/*class demo {
+class demo {
 	public static void (string args 
 		[]
 	) (
 	classout.println("OOPS");
 	)
-	}*/
-	
-	public class oopsBannerUC2 {
-
-    public static void main(String[] args) {
-
-        System.out.println("*****   *****   ******   ***** ");
-        System.out.println("*   *   *   *   *    *   *    *");
-        System.out.println("*   *   *   *   *    *   *    *");
-        System.out.println("*   *   *****   ******   ***** ");
-        System.out.println("*   *   *   *   *        *     ");
-        System.out.println("*   *   *   *   *        *     ");
-        System.out.println("*****   *   *   *        *     ");
-    }
-}
+	}
