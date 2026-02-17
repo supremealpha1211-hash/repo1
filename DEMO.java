25b0748c8655940c5ee284c43e253b915fa6b004
Raiyan kammal <supremealpha1211@gmail.com>
2026-02-17 15:36:10 +0530

    Completed UC2

--- DEMO.java
+++ DEMO.java
@@ -1,5 +1,21 @@
-class demo {
-	public static void (string) (
+/*class demo {
+	public static void (string args 
+		[]
+	) (
 	classout.println("OOPS");
 	)
-	}
+	}*/
+	
+	public class oopsBannerUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("*****   *****   ******   ***** ");
+        System.out.println("*   *   *   *   *    *   *    *");
+        System.out.println("*   *   *   *   *    *   *    *");
+        System.out.println("*   *   *****   ******   ***** ");
+        System.out.println("*   *   *   *   *        *     ");
+        System.out.println("*   *   *   *   *        *     ");
+        System.out.println("*****   *   *   *        *     ");
+    }
+}
